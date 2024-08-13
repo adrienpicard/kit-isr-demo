584b04656019fd9e9d9f6f35ebe7e17eea02d980
Adrien Picard <adrien@a-p.studio>
2024-08-13 09:33:10 +0100

    About page

---
+++ src/routes/about/+page.server.ts
@@ -0,0 +1,7 @@
+export const trailingSlash = 'always';
+
+export const config = {
+	isr: {
+		expiration: 60
+	}
+};
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Menu from '../../components/menu.svelte';
+</script>
+
+<h1>About</h1>
+
+<Menu />
