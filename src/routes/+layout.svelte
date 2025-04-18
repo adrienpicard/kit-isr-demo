71dc4028940b2fe67d69570e5b2a848f7e88aa2b
Adrien Picard <adrien@a-p.studio>
2025-04-18 17:43:29 +0100

    Update demo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,10 @@
 <script>
 	const { children } = $props();
-
-	const invalidate = () => {
-		fetch('/yay', {
-			headers: { 'x-prerender-revalidate': 'ec04d5c6-2fc3-446a-9879-5dbe00bf3730' }
-		});
-		fetch('/nay', {
-			headers: { 'x-prerender-revalidate': 'ec04d5c6-2fc3-446a-9879-5dbe00bf3730' }
-		});
-	};
 </script>
 
 <ul class="flex gap-2 p-2">
-	<li><a href="/yay">/yay</a></li>
-	<li><a href="/nay">/nay</a></li>
+	<li><a href="/route-a">/route-a</a></li>
+	<li><a href="/route-b">/route-b</a></li>
 </ul>
 
 {@render children()}
-
-<button onclick={invalidate}>Invalidate both pages</button>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	const invalidate = () => {
+		fetch('/route-a', {
+			headers: { 'x-prerender-revalidate': 'ec04d5c6-2fc3-446a-9879-5dbe00bf3730' }
+		});
+		fetch('/route-b', {
+			headers: { 'x-prerender-revalidate': 'ec04d5c6-2fc3-446a-9879-5dbe00bf3730' }
+		});
+	};
+</script>
+
+<button onclick={invalidate}>Invalidate pages</button>
