11f66f7572575c051225a40bfd5c67034bd0c031
Adrien Picard <adrien@a-p.studio>
2025-04-23 11:35:40 +0100

    Test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,13 @@
 		fetch('/route-b', {
 			headers: { 'x-prerender-revalidate': 'ec04d5c6-2fc3-446a-9879-5dbe00bf3730' }
 		});
+
+		fetch('/route-a/__data.json', {
+			headers: { 'x-prerender-revalidate': 'ec04d5c6-2fc3-446a-9879-5dbe00bf3730' }
+		});
+		fetch('/route-b/__data.json', {
+			headers: { 'x-prerender-revalidate': 'ec04d5c6-2fc3-446a-9879-5dbe00bf3730' }
+		});
 	};
 </script>
 
